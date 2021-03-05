35f06cda6ce4d8e8bf2abd674c27e289ebad2b80
Maxim Yazykov <yazykovmaxim@gmail.com>
2021-03-05 17:01:17 +0300

    feat: create solution of task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  if (array === undefined || array.length == 0) {
+      return  array = 0;
+    }else{
+      array.sort((a, b) => {
+        if (a < b) {
+          return -1;
+        } else if (a > b) {
+          return 1;
+        } else if (a == b) {
+          return 0;
+        }
+      })
+      return array[0]
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return array = 0;
+  } else {
+    array.sort((a, b) => {
+      if (a < b) {
+        return -1;
+      } else if (a > b) {
+        return 1;
+      } else if (a == b) {
+        return 0;
+      }
+    })
+    return array[array.length - 1]
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return array = 0;
+  } else {
+    let sum = array.reduce((previous, current, array) => {
+      return previous+= current;
+    }, 0)
+    return sum/array.length;
+  } 
 }
